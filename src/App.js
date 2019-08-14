aef1ffcc9fb018faeda4e5c04a045fd2d43d9650
JORDY DELGADO <jordydelgado@hotmail.com>
2019-08-13 20:41:15 -0500

    Creado el proyecto en React

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <div>
+        <p>Header top </p>
+      </div>
+      <div>
+        <p>Header main</p>
+      </div>
+      <div>
+        <p>Banner</p>
+      </div>
+      <div>
+        <p>Buscador</p>
+      </div>
+      <div>
+        <p>Contenido</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
